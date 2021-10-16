fa2caa64ba7628123335f6b7fd3f5701d956ac58
dnslin <bolekmiss@gmail.com>
2021-10-16 18:48:46 +0800

    第一次提交,创建项目 暂时搁置！！！

---
+++ src/main/java/com/dnslin/onemanagerjava/OnemanagerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dnslin.onemanagerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemanagerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnemanagerJavaApplication.class, args);
+    }
+
+}
