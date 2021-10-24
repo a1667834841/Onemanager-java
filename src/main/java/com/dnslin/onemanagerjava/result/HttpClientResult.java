77015f2e486d21a93bd7bf0396f6e488168d92b7
dnslin <1636988859@qq.com>
2021-10-24 14:50:58 +0800

    增加httpclient工具类

---
+++ src/main/java/com/dnslin/onemanagerjava/result/HttpClientResult.java
@@ -0,0 +1,38 @@
+package com.dnslin.onemanagerjava.result;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class HttpClientResult {
+    private static final long serialVersionUID = 2168152194164783950L;
+
+    /**
+     * 响应状态码
+     */
+    private int code;
+
+    /**
+     * 响应数据
+     */
+    private String content;
+
+    public HttpClientResult() {
+    }
+
+    public HttpClientResult(int code) {
+        this.code = code;
+    }
+
+    public HttpClientResult(String content) {
+        this.content = content;
+    }
+
+    public HttpClientResult(int code, String content) {
+        this.code = code;
+        this.content = content;
+    }
+
+
+}
