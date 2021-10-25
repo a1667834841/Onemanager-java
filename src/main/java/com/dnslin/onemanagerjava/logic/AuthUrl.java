5d859302d49a4696dec2a0a33072ea49fb8733e0
dnslin <1636988859@qq.com>
2021-10-25 13:11:03 +0800

    AuthURL的单元测试

---
+++ src/main/java/com/dnslin/onemanagerjava/logic/AuthUrl.java
@@ -0,0 +1,14 @@
+package com.dnslin.onemanagerjava.logic;
+
+public interface AuthUrl {
+
+    /**
+     * 功能描述
+     * @author dnslin
+     * @date 2021/10/25
+     * @param clientId 客户端ID
+     * @param redirectUri 回调URL
+     * @return AuthUrl 重定向url
+     */
+    String getAuthUrl(String clientId, String redirectUri);
+}
--- src/test/java/com/dnslin/onemanagerjava/test/Test01.java
+++ src/test/java/com/dnslin/onemanagerjava/test/Test01.java
@@ -1,10 +1,15 @@
 package com.dnslin.onemanagerjava.test;
 
+import cn.hutool.core.util.RandomUtil;
+import com.dnslin.onemanagerjava.result.HttpClientResult;
 import com.dnslin.onemanagerjava.utils.HttpClientUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -12,6 +17,25 @@ public class Test01 {
     @Test
     public void test(){
         System.out.println("test run .....");
-        HttpClientUtils.doGet("https://baidu.com");
+        Map<String, String> map = new HashMap<>();
+        map.put("foo", "bar");
+        HttpClientResult httpClientResult = HttpClientUtils.doGet("https://baidu.com",map);
+        System.out.println(httpClientResult.getContent());
+        System.out.println(httpClientResult.getCode());
+    }
+
+    @Test
+    public void test01(){
+        String clientId = "2ddd8ff9-1137-407b-91a0-58c4dc661968";
+        String redirectUri = "http://localhost:5678/api/auth";
+        String state = RandomUtil.randomString(5);
+        String url = String.format("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+                "client_id=%s" +
+                "&response_type=code" +
+                "&redirect_uri=%s" +
+                "&response_mode=query" +
+                "&scope=files.readwrite.all files.readwrite offline_access" +
+                "&state=%s", clientId, redirectUri, state);
+        System.out.println(url);
     }
 }
