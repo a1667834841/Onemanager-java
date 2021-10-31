baca28106ffe8f9ab83d14002ecb32024926b4e9
dnslin <1636988859@qq.com>
2021-10-31 18:24:19 +0800

    1.启动类增加mapper扫描bean注解 2.增加mapper类bean注解

--- src/main/java/com/dnslin/onemanager/OnemanagerJavaApplication.java
+++ src/main/java/com/dnslin/onemanager/OnemanagerJavaApplication.java
@@ -1,9 +1,11 @@
 package com.dnslin.onemanager;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.dnslin.onemanager.mapper")
 public class OnemanagerJavaApplication {
 
     public static void main(String[] args) {
