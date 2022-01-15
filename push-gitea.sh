701e6f9ee0464d767b182e0986bc282e7f28c28b
a1667834841 <1667834841@qq.com>
2022-01-16 00:50:30 +0800

    ci 提交gitea仓库

--- push-gitea.sh
+++ push-gitea.sh
@@ -6,4 +6,5 @@ codingUrl="https://gitea.dongshanxia.top:35000/ggball/onemanager-java.git"
 git init
 git add -A
 git commit -m "${msg}"
+git remote add origin $codingUrl
 git push -f $codingUrl master # 推送到coding
