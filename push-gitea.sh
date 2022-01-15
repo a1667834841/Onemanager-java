9e832edba7f5b12c9682b51b98292b0cd10a9ddb
ggball <1667834841@qq.com>
2022-01-16 01:09:37 +0800

    ci 提交gitea仓库

--- push-gitea.sh
+++ push-gitea.sh
@@ -3,6 +3,9 @@ git config --global user.email "1667834841@qq.com"
 
 codingUrl="https://gitea.dongshanxia.top:35000/ggball/onemanager-java.git"
 
+
 git add -A
 git commit -m "${msg}"
+git remote add origin $codingUrl
+git show-ref
 git push "https://gitea.dongshanxia.top:35000/ggball/onemanager-java.git" master --force # 推送
