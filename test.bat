679278d9817b01784494480e3170839a30100698
ggball <1667834841@qq.com>
2022-01-16 00:57:13 +0800

    ${msg}

--- test.bat
+++ test.bat
@@ -3,8 +3,7 @@ git config --global user.email "1667834841@qq.com"
 
 codingUrl="https://gitea.dongshanxia.top:35000/ggball/onemanager-java.git"
 
-git checkout
 git add -A
 git commit -m "${msg}"
-git remote add origin $codingUrl
+git remote add origin "https://gitea.dongshanxia.top:35000/ggball/onemanager-java.git"
 git push -f origin master # 推送
