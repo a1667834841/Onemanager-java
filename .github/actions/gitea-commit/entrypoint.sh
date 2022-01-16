2028e8dc6a902dc4762991e64d5798e0183837f5
ggball <1667834841@qq.com>
2022-01-16 15:10:50 +0800

    ci 提交gitea仓库

--- .github/actions/gitea-commit/entrypoint.sh
+++ .github/actions/gitea-commit/entrypoint.sh
@@ -12,7 +12,7 @@ git config --global user.email "$GIT_EMAIL"
 git init
 git add .
 git commit -m "${msg}"
-git push -f $GIT_REPO master
+git push -f https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git master
 
 
 echo "::set-output name=repo::$GIT_REPO"
