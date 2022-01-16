cf2fb99eb9744669b6d81fac13c8fab0ac0848cf
ggball <1667834841@qq.com>
2022-01-16 13:04:28 +0800

    test

--- test.bat
+++ test.bat
@@ -4,5 +4,5 @@ git config --global user.email "1667834841@qq.com"
 git init
 git add -A
 git commit -m "test"
-git remote add origin https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git
+git remote add gitea https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git
 git push https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git master  # 推送
