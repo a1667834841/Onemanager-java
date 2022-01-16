e3cbc68ea97b72bb1f72acc230d17471160d6f68
ggball <1667834841@qq.com>
2022-01-16 13:34:08 +0800

    ci 提交gitea仓库

--- .github/actions/gitea-commit/entrypoint.sh
+++ .github/actions/gitea-commit/entrypoint.sh
@@ -9,7 +9,7 @@ GIT_EMAIL="${INPUT_EMAIL}"
 git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
 
-
+git init
 git add .
 git commit -m "${msg}"
 git push -f $GIT_REPO master
