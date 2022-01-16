f526fbaa19abab1adcec224a5aef515a2a561dcc
ggball <1667834841@qq.com>
2022-01-16 13:04:41 +0800

    test

--- test.bat
+++ test.bat
@@ -5,4 +5,4 @@ git init
 git add -A
 git commit -m "test"
 git remote add gitea https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git
-git push https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git master  # 推送
+git push -f https://ggball:bc65cc90c76baba247e753c95ef5721200e97b59@gitea.dongshanxia.top:35000/ggball/onemanager-java.git master  # 推送
